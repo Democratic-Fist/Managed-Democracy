bb546217a4427936aba64b23e04328aa0f0f32fe
Super Earth Armed Forces - SEAF <jose.lino.chaves@escola.pr.gov.br>
2024-09-03 15:17:16 -0300

    add files js and css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(855, 705);
+    background("white");
+  }
+  
+  function draw() {
+    
+    stroke("black");
+    fill("black");
+  
+  
+     if(mouseIsPressed) {
+    circle(mouseX, mouseY, 35);
+      
+    }  
+    
+  }
+  
